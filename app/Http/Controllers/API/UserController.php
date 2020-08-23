a2f65bc6a771ea80c392f08c2f501640bcbc5ac1
Thet Minn Htun <thetminnhtun92@gmail.com>
2020-08-23 11:16:44 +0630

    API

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::orderBy('id', 'desc')->paginate(10);
+        return response()->json(compact('users'), Response::HTTP_OK);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // User::create($request->only('name', 'email', 'password'));
+        // return User::create($request->all());
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        return response()->json(compact('user'), 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = User::find($id);
+        return response()->json(compact('user'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        // return User::find($id)->update($request->all());
+        $user = User::find($id);
+        if(! $user) {
+            return response()->json(['message' => 'No user.'], 404);
+        } 
+        $user->update($request->all());
+        return response()->json(compact('user'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        // return User::find($id)->delete();
+        // return User::destroy($id);
+        $user = User::find($id);
+        if(! $user) {
+            return response()->json(['message' => 'No User.'], 404);
+        }
+        $user->delete();
+        return response()->json(compact('user'));
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('user', 'API\UserController@index');
+Route::post('user', 'API\UserController@store');
+Route::put('user/{id}', 'API\UserController@update');
+Route::delete('user/{id}', 'API\UserController@destroy');
